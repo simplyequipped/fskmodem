9281d6c05fa0ee7be792aa23a8a30bae7124304b
Howard G <howard@simplyequipped.com>
2022-03-06 20:54:26 -0500

    updated test script

--- tests/test_modem.py
+++ tests/test_modem.py
@@ -1,16 +1,13 @@
 import minimodem
 
+def rx_callback(data):
+    print(data.decode('utf-8'))
+
 alsa_device = minimodem.get_alsa_device('USB PnP')
 if alsa_device is None:
     print('No alsa device found')
     exit()
 
 modem = minimodem.Modem(alsa_device)
-#modem.send('hello world!')
-
-#input('Press enter to stop modems\n')
-#modem.stop()
-
-#input('Press enter to quit test\n')
-
+modem.set_rx_callback(rx_callback)
 
