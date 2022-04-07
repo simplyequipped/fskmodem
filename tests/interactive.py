8d8e487d442cd9b6c7255f7f0e49d75b2fbf5bda
Howard G <howard@simplyequipped.com>
2022-04-06 20:40:33 -0400

    update test scripts

--- tests/interactive.py
+++
@@ -1,8 +0,0 @@
-import minimodem
-
-modem = minimodem.Modem()
-
-def rx_callback(data):
-    print(data)
-
-modem.set_rx_callback(rx_callback)
--- tests/test_modem.py
+++ tests/test_modem.py
@@ -3,11 +3,6 @@ import minimodem
 def rx_callback(data):
     print('\nPacket received: ' + data.decode('utf-8'))
 
-alsa_device = minimodem.get_alsa_device('USB PnP')
-if alsa_device is None:
-    print('No alsa device found')
-    exit()
-
-modem = minimodem.Modem(alsa_device)
+# use system default audio device
+modem = minimodem.Modem()
 modem.set_rx_callback(rx_callback)
-
