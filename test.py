bc9ccf889c119f1f87882ea10de6901ccdf250db
OtisByron <howard@simplyequipped.com>
2023-09-22 23:56:45 -0400

    testing changes

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import fskmodem
 def ptt():
     pass
 
-modem = fskmodem.Modem(baudmode=100)
+modem = fskmodem.Modem(baudmode=100, sync_byte=None)
+#modem = fskmodem.Modem(baudmode=100, sync_byte=None)
 modem._debug = True
 modem.set_ptt_callback(ptt)
 
