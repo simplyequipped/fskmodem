88267e9789c46ede510fb1b08cbfb0f0b004edaf
OtisByron <howard@simplyequipped.com>
2023-09-22 21:30:03 -0400

    fix imports

--- fskmodem/__init__.py
+++ fskmodem/__init__.py
@@ -1,6 +1,5 @@
+from fskmodem.modem import HDLC
 from fskmodem.modem import FSKBase
 from fskmodem.modem import FSKTransmit
 from fskmodem.modem import FSKReceive
 from fskmodem.modem import Modem
-from fskmodem.modem import HDLC
-from fskmodem.modem import get_alsa_device
