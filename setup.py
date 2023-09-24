1ffc8172e48a24450881ed235497d9980cfec3b2
OtisByron <howard@simplyequipped.com>
2023-09-24 16:44:33 -0400

    update version, consistent format and structure

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="fskmodem",
-    version="0.1.0",
-    author="Simply Equipped LLC",
-    author_email="howard@simplyequipped.com",
-    description="Full duplex FSK modem",
+setup(
+    name='fskmodem',
+    version='0.2.0',
+    author='Simply Equipped LLC',
+    author_email='howard@simplyequipped.com',
+    description='Full duplex AFSK modem using the minimodem application',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/simplyequipped/fskmodem",
+    long_description_content_type='text/markdown',
+    url='https://github.com/simplyequipped/fskmodem',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6'
 )
+
